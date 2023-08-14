ac68c6742968f7cb42c55db2e70c258dc8397608
Alok905 <alokranjanjoshi07567@gmail.com>
2023-08-14 17:05:35 +0530

    full

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -13,7 +13,6 @@ const Trending = () => {
     const { data } = await axios.get(
       `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
     );
-
     setContent(data.results);
   };
 
